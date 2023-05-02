d68395348c6b20aa42f36cb8b7243fe4713b470a
Nikita Levushkin <therederickmind@gmail.com>
2023-05-02 21:18:55 +0300

    Changed nginx config, README and ALLOWED HOSTS

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -12,7 +12,10 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '158.160.19.189',
+    'https://rdm-yatube.ddns.net/'
+]
 
 # Application definition
 
